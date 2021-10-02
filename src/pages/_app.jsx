e04ead62c5b65e69699256bb6dcf0f12337087e3
ibqn <evgen.ibqn@gmail.com>
2021-10-02 23:27:34 +0200

    add global style

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,22 +1,6 @@
-import 'styles/globals.css'
-
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
-
-const styled = { createGlobalStyle }
-
-const GlobalStyle = styled.createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`
-
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+import { ThemeProvider } from 'styled-components'
+import GlobalStyle from 'components/global-style'
+import theme from 'theme'
 
 function MyApp({ Component, pageProps }) {
   return (
