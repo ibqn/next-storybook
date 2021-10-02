bc89370564a0b25a5e8deaea63e3489b8199bd83
ibqn <evgen.ibqn@gmail.com>
2021-10-02 16:59:03 +0200

    add styled components

---
+++ src/pages/_app.jsx
@@ -0,0 +1,32 @@
+import 'styles/globals.css'
+
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+
+const styled = { createGlobalStyle }
+
+const GlobalStyle = styled.createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+}
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default MyApp
